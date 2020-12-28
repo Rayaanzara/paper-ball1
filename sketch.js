12763d8cca29a0899c4ed3895922dadd105810ee
Rayaanzara <73578075+Rayaanzara@users.noreply.github.com>
2020-12-28 17:12:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1200, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(600,680,1200,20)
+	
+	wall1 = new Dustbin(800,600,20,100);
+    wall2 = new Dustbin(1020,600,20,100);
+    wall3 = new Dustbin(910,660,240,20);
+
+	ball  = new Ball(200,450,40);
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  ground.display();
+  wall1.display();
+  wall2.display();
+  wall3.display();
+
+  ball.display();
+
+
+  drawSprites();
+ 
+}
+
+
+function keyPressed() {
+
+if(keyCode === UP_ARROW){
+Matter.Body.applyForce(ball.body,ball.body.position,{x:70,y:-85});
+
+}
+
+
+}
